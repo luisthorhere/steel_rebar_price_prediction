79201124a918226adaffe3162d15975b9a6e1146
luisthorhere <luisthorhere@gmail.com>
2025-11-09 11:36:04 -0600

    Verify api key header

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -2,8 +2,10 @@ from fastapi import Header, HTTPException, status
 from .rate_limiter import check_rate_limit
 from ..config.config import SETTINGS
 from ..logger.logger import logger
+
+
 async def verify_api_key(x_api_key: str = Header(...)):
-    
+
     if x_api_key not in SETTINGS.api_key:
         logger.info(f"Invalid API_KEY: {x_api_key}")
         raise HTTPException(
